eea16396b02100e980231af87624e76d7f165735
AlexBizPlus <alexeynejnov@gmail.com>
2020-02-28 12:36:44 +0700

    Подготовка к защите
    Работа над  замечаниями
    Сделаны модальные окна на js
    Проверка на валидацию
    Pixel Perfect

--- js/map.js
+++ js/map.js
@@ -1,22 +1,22 @@
-var mapLink = document.querySelector(".map-link");
-var mapPopup = document.querySelector(".map");
-var mapClose = mapPopup.querySelector(".modal-close");
+var mapLink = document.querySelector('.map-link');
+var mapPopup = document.querySelector('.map');
+var mapClose = mapPopup.querySelector('.modal-close');
 
-mapLink.addEventListener("click", function (evt) {
+mapLink.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mapPopup.classList.add("modal-show");
+  mapPopup.classList.add('modal-show');
 });
 
-mapClose.addEventListener("click", function (evt) {
+mapClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
+  mapPopup.classList.remove('modal-show');
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
+    if (mapPopup.classList.contains('modal-show')) {
+      mapPopup.classList.remove('modal-show');
     }
   }
 });
